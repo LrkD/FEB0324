97f274f189ad2364b5a35d077709fd9e9c3f2de8
John Jaca <johnangelo.jaca@letran.edu.ph>
2024-02-03 14:45:21 +0800

    account to ni karl ewan ko lang kung bakit iba

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/server/server.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styleUrl: '../../../node_modules/bootstrap/dist/css/bootstrap.min.css'
+})
+export class ServerComponent {
+    serverName="Server name";
+    disabledButton: boolean = false;
+    serverStatus: string = "";
+    resetServerName(){
+      this.serverName="";
+      this.disabledButton = true;
+    }
+
+    setServerName(event: Event){
+      this.disabledButton = false;
+      this.serverName = (<HTMLInputElement>event.target).value;
+    }
+
+    addServer(){
+      this.serverStatus = "A server with a name " + this.serverName + " has been added";
+    }
+}
